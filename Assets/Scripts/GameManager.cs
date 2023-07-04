f3753eea3eaad12d1e219462a890afafdf936791
이성우 <lsw5244@naver.com>
2023-07-04 21:12:47 +0900

    feat : 게임 실행 시 MineBlock을 생성하도록 구현
    게임 실행 시 MineBlock을 12*20만큼 GameBackground위에 생성하도록 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mineBlockPrefabs;
+
+    private int boardWidthLength = 12;
+    private int boardHeightLength = 20;
+    private int mineCount = 56;
+    private GameObject[,] mineBlocks;
+
+    void Start()
+    {
+        mineBlocks = new GameObject[boardHeightLength, boardWidthLength];
+        SpawnMineBlock();
+    }
+
+    void SpawnMineBlock()
+    {
+        // 처음으로 MineBlock이 생성 될 위치
+        Vector2 firstPos = new Vector2(-2.2f, 3.2f);
+        float blockSize = mineBlockPrefabs.transform.localScale.x;
+
+        for (int i = 0; i < boardHeightLength; ++i)
+        {
+            for (int j = 0; j < boardWidthLength; ++j)
+            {
+                mineBlocks[i, j] = Instantiate(mineBlockPrefabs);
+                mineBlocks[i, j].transform.position
+                    = new Vector2(firstPos.x + blockSize * j, firstPos.y - blockSize * i);
+            }
+        }
+    }
+}
